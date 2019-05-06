96cd88ed67ce4a6a15756d1f730d5f230348574f
drii-arthur <47278146+drii-arthur@users.noreply.github.com>
2019-05-06 21:57:10 +0700

    Initial commit

---
+++ src/create.js
@@ -0,0 +1,10 @@
+const db = require("./db");
+const args = process.argv.slice(2);
+const query = `INSERT INTO contacts(fullName,phoneNumber,email,gender) VALUES ('${
+  args[0]
+}',${args[1]}','${args[2]}','${args[3]}')`;
+
+db.run(query, function(err) {
+  if (err) throw err;
+  console.log("successfully created a new row");
+});
